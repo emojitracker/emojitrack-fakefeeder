6428f6db10bd9c46c3cbc608e0f2f0af954afd78
Matthew Rothenberg <mrothenberg@gmail.com>
2020-06-12 17:45:16 -0400

    chore: cleanup main

--- main.go
+++ main.go
@@ -1,65 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
-	"math/rand"
-	"strconv"
 	"strings"
 	"time"
 
-	"github.com/icrowley/fake"
 	"github.com/mroth/weightedrand"
 )
 
-var targetURL = flag.String("target", "redis://localhost:6379", "URI for redis target")
-var rate = flag.Int("rate", 250, "number of updates per second to generate")
-var weighted = flag.Bool("weight", true, "weight random update probability based on history")
-var verbose = flag.Bool("v", false, "verbose log all updates to stdout")
-
-// Placeholder for snapshot score values
-// var initialScores = map[string]int{}
-
-// a semi realistic scale of where tweet IDs currently are at
-var globalTweetID = 769706198425825280
-
-// EnsmallenedTweet matches the structure used by emojitrack-feeder for sending
-// out bandwidth efficient tweets.
-type EnsmallenedTweet struct {
-	ID              string    `json:"id"`
-	Text            string    `json:"text"`
-	ScreenName      string    `json:"screen_name"`
-	Name            string    `json:"name"`
-	Links           []string  `json:"links"`
-	ProfileImageURL string    `json:"profile_image_url"`
-	CreatedAt       time.Time `json:"created_at"`
-}
-
-func (t *EnsmallenedTweet) Encoded() []byte {
-	b, _ := json.Marshal(t)
-	return b
-}
-
-// pick a random ID for an existing emoji in counter
-func randomEmoji() *emojiRanking {
-	return &emojiRankings[rand.Intn(len(emojiRankings))]
-}
-
-func randomUpdateForEmoji(e emojiRanking) EnsmallenedTweet {
-	globalTweetID += 42
-
-	return EnsmallenedTweet{
-		ID:              strconv.Itoa(globalTweetID),
-		Text:            fmt.Sprintf("%s %s", fake.Sentence(), e.char),
-		ScreenName:      fake.UserName(),
-		Name:            fake.FullName(),
-		Links:           []string{},
-		ProfileImageURL: "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
-		CreatedAt:       time.Now(),
-	}
-}
+var (
+	targetURL = flag.String("target", "redis://localhost:6379", "URI for redis target")
+	rate      = flag.Int("rate", 250, "number of updates per second to generate")
+	weighted  = flag.Bool("weight", true, "weight random update probability based on history")
+	verbose   = flag.Bool("v", false, "verbose log all updates to stdout")
+)
 
 func main() {
 	flag.Parse()
@@ -91,7 +46,7 @@ func main() {
 	for _, er := range emojiRankings {
 		for i := 0; i < 10; i++ {
 			t := randomUpdateForEmoji(er)
-			updateScript.Do(c, er.id, t.Encoded())
+			updateScript.Do(c, er.id, t.MustEncode())
 		}
 	}
 
@@ -117,7 +72,7 @@ func main() {
 		}
 
 		t := randomUpdateForEmoji(e)
-		updateScript.SendHash(c, e.id, t.Encoded())
+		updateScript.SendHash(c, e.id, t.MustEncode())
 		c.Flush()
 
 		if *verbose {
