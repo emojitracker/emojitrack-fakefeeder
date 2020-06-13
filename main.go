184ffb2523426dd1beadc9af1189cbbbad070873
Matthew Rothenberg <mrothenberg@gmail.com>
2020-06-13 14:21:20 -0400

    refactor: restructure Feeder into package

--- main.go
+++ main.go
@@ -1,81 +1,56 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"math/rand"
+	"os"
 	"strings"
 	"time"
-
-	"github.com/mroth/weightedrand"
 )
 
 var (
 	targetURL = flag.String("target", "redis://localhost:6379", "URI for redis target")
-	rate      = flag.Int("rate", 250, "number of updates per second to generate")
+	rate      = flag.Uint("rate", 250, "number of updates per second to generate")
 	weighted  = flag.Bool("weight", true, "weight random update probability based on history")
-	verbose   = flag.Bool("v", false, "verbose log all updates to stdout")
+	verbose   = flag.Bool("v", false, "verbose log all feeder updates")
 )
 
 func main() {
 	flag.Parse()
-
-	// echo startup status
-	log.Printf("Starting up. TARGET_URL: %v (rate: %d/sec.)\n", *targetURL, *rate)
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+	logger.Printf("Starting up. Target: %v (rate: %d/sec.)\n", *targetURL, *rate)
 
 	// paranoid safety check: refuse to go anywhere near a production DB
 	if strings.Contains(*targetURL, "rediscloud") {
-		log.Fatal("Are you certain you aren't trying to hit a prod db?")
+		logger.Fatal("Are you certain you aren't trying to hit a prod db?")
 	}
 
-	// try to connect to Redis, otherwise die with error
-	initRedis()
-	c := redisPool.Get()
-	defer c.Close()
-	if _, err := c.Do("PING"); err != nil {
-		log.Fatal("Redis connection failed: ", err)
-	}
-
-	// set up initial state in redis
-	log.Println("Setting up initial state...")
-	// reset counts to equal snapshot
-	err := seedScores(c)
+	// otherwise, set up the redis pool
+	pool, err := targetPool(*targetURL)
 	if err != nil {
-		log.Fatal("FATAL: could not seed initial scores: ", err)
-	}
-	// mock historic tweet sliding window for each emoji
-	err = seedTweets(c)
-	if err != nil {
-		log.Fatal("FATAL: could not seed initial tweets: ", err)
+		logger.Fatal(err)
 	}
 
-	// create weighted random choice data
-	var emojiChoices []weightedrand.Choice
-	for _, er := range emojiRankings {
-		ec := weightedrand.Choice{Item: er, Weight: uint(er.score)}
-		emojiChoices = append(emojiChoices, ec)
+	// don't forget to seed random!
+	rand.Seed(time.Now().UnixNano())
+
+	// set up feeder with initial state in redis
+	logger.Println("Setting up initial feeder state...")
+	feeder, err := NewFeeder(pool, emojiRankings, *weighted)
+	if err != nil {
+		logger.Fatal(err)
 	}
-	emojiChooser := weightedrand.NewChooser(emojiChoices...)
+	if *verbose {
+		feeder.VerboseLogger = logger
+	}
 
 	// start feeding redis random updates
 	period := time.Second / time.Duration(*rate)
-	log.Println("Now starting to send fake updates every", period)
-	_ = updateScript.Load(c)
-	for {
-		time.Sleep(period)
-
-		var e emojiRanking
-		if *weighted {
-			e = emojiChooser.Pick().(emojiRanking)
-		} else {
-			e = *randomEmoji()
-		}
-
-		t := randomUpdateForEmoji(e)
-		updateScript.SendHash(c, e.id, t.MustEncode())
-		c.Flush()
-
-		if *verbose {
-			log.Println("Sent fake update for", e)
-		}
+	logger.Printf("Sending fake updates every %v (%v/sec)", period, *rate)
+	errChan := feeder.Start(context.Background(), period)
+	for err := range errChan {
+		logger.Println("ERROR:", err)
 	}
 }
--- redis.go
+++ redis.go
@@ -1,37 +1,34 @@
 package main
 
 import (
-	"log"
+	"errors"
 	"net/url"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
 
-var (
-	redisPool *redis.Pool
-)
-
-func initRedis() {
-	url, err := url.Parse(*targetURL)
+// parse target redis URL to extract host and password (redigo doesn't natively
+// understand URI format), use that for instantiating newPool().
+func targetPool(targetURL string) (*redis.Pool, error) {
+	url, err := url.Parse(targetURL)
 	if err != nil {
-		log.Fatal("Could not understand TARGET_URL paramer. Dying.")
+		return nil, errors.New("could not parse target URL")
 	}
 	host := url.Host
 	password := ""
 	if url.User != nil {
 		password, _ = url.User.Password()
 	}
-	redisPool = newPool(host, password)
+	return newPool(host, password), nil
 }
 
-// yay for standard boilerplate, sigh
 func newPool(server, password string) *redis.Pool {
 	return &redis.Pool{
 		MaxIdle:     3,
 		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
-			log.Println("REDISPOOL: Dialing a new Redis connection...")
+			// log.Println("REDISPOOL: Dialing a new Redis connection...")
 			c, err := redis.Dial("tcp", server)
 			if err != nil {
 				return nil, err
@@ -44,41 +41,10 @@ func newPool(server, password string) *redis.Pool {
 			}
 			return c, err
 		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			log.Println("REDISPOOL: testing on borrow")
-			_, err := c.Do("PING")
-			return err
-		},
+		// TestOnBorrow: func(c redis.Conn, t time.Time) error {
+		// 	log.Println("REDISPOOL: testing on borrow")
+		// 	_, err := c.Do("PING")
+		// 	return err
+		// },
 	}
 }
-
-// This is the same update script used in emojitrack-feeder
-var updateScript = redis.NewScript(0, `
--- Updates the server whenever a new emoji is seen in a tweet
---
--- Putting this in a script enables us to save some bandwidth by not
--- transmitting any redundant data to the server, as we can calculate the
--- appropriate key names there and re-use data that goes to multiple
--- destinations.
-
-local uid      = ARGV[1]   -- unified codepoint ID
-local tinyjson = ARGV[2]   -- json blob representing the ensmallened tweet
-
--- increment the score in a sorted set
-redis.call('ZINCRBY', 'emojitrack_score', 1, uid)
-
--- stream the fact that the score was updated
-redis.call('PUBLISH', 'stream.score_updates', uid)
-
--- for each emoji char, store the most recent 10 tweets in a list
-local tweet_details_key = "emojitrack_tweets_" .. uid
-redis.call('LPUSH', tweet_details_key, tinyjson)
-redis.call('LTRIM', tweet_details_key, 0, 9)
-
--- also stream all tweet updates to named streams by char
-local stream_details_key = "stream.tweet_updates." .. uid
-redis.call('PUBLISH', stream_details_key, tinyjson)
-
--- return ok status
-return 1
-`)
